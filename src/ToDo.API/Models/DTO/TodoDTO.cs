e722c9ce7d4db2d542c2b894d89916b9d246f554
Christian Benedict R. Gantuangco <gantuangcoc98@gmail.com>
2024-06-21 16:17:55 +0800

    Removed files

--- src/ToDo.API/Models/DTO/TodoDTO.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace Todo.API.DTO;
-
-public record class TodoDTO(
-    [Required]
-    [StringLength(250)] 
-    string? Task
-);
--- src/ToDo.API/Models/Todo.cs
+++
@@ -1,7 +0,0 @@
-namespace Todo.API.Models;
-
-public record ToDo
-{
-    public int Id { get; set; }
-    public string? Task { get; set; }
-}
